3e41669eab19ac759e08bd3ae464318fa451ff71
Mike Shelton <Mike@sheltonstudios.net>
2016-10-01 14:30:15 -0500

    API calls functioning

--- devicetypes/sheltonst/lightify.src/lightify.groovy
+++ devicetypes/sheltonst/lightify.src/lightify.groovy
@@ -31,6 +31,7 @@ metadata {
 		input "password", "password", title: "Password", description: "Lightify Password", required: true
 		input "serial", "text", title: "Serial", description: "Gateway Serial Number", required: true		
 		input "uuid", "text", title: "Dropcam Device ID", description: "Legacy Dropcam Code"
+		input "fade", "20", title: "Fade seconds", description: "Transition time in milliseconds."
 	}
     
     tiles {
@@ -65,48 +66,29 @@ metadata {
 
 //[MS]
 def getLightifyStatus(){    
-	//log.debug "in private function"
-	getCurrentState('lightify-1')
+
+    //Separate the calls out to make it easier
+    // 1. Login
+    MikeLogin3()
+    ////log.debug "post MikeLogin3"
+    ////log.debug "Token still available? -- ${state.auth}"
+    
+    // 2. Get light details
+	getCurrentState('lightify-5')
+    
 }
 
 
 
-
-
-
-
-def parse5(Map response) {
-	def result = []
-	if (response.status == 403) {
-		// forbidden and either missing cookie or it's expired, get a new one
-        log.debug "403"
-		result << login()
-	} else if (response.status == 400) {
-        log.debug "400 - Logged out"
-	} else if (response.status == 200) {
-		if (response.headers.'Content-Type'.contains("image/jpeg")) {
-			def imageBytes = response.data
-			if (imageBytes) {
-				storeImageInS3(getPictureName(), imageBytes)
-			}
-		} else if (response.headers.'Content-Type'.contains("application/json")) {
-			result << response.body
-		}
-	}
-	log.debug result
-	result
-
-}
-
-
+//Something seems broken with these. Had to hard code the values for logging in to work.
 private getUsername() {
-	getDevicePreferenceByName(device, "username")
+	return getDevicePreferenceByName(device, "username")
 }
 private getPassword() {
-	getDevicePreferenceByName(device, "password")
+	return getDevicePreferenceByName(device, "password")
 }
 private getSerial(){
-	getDevicePreferenceByName(device, "serial")	
+	return getDevicePreferenceByName(device, "serial")	
 }
 
 
@@ -115,163 +97,228 @@ private getSerial(){
 
 //[MS] Lightify Login
 private login(){
-	log.debug "Logging in"
-	rest(
-			method: 'POST',
-			endpoint: "https://us.lightify-api.org",
-			path: "/lightify/services/session",
-			body: [username: getUsername(), password: getPassword(), serialNumber : getSerial()],
-			headers: ['Content-Type': 'application/json'],
-			//requestContentType: "application/x-www-form-urlencoded",
-			synchronous: true
-	)
 }
 
-//[MS] Trying login different way with physicalgraph
-private loginMike(){
-	def httpRequest = [
+
+def MikeLogin3(){
+	log.debug "MikeLogin3"
+    
+    
+    log.debug getUsername()
+    log.debug getPassword()
+    log.debug getSerial()
+    
+    
+	def params = [
 			method: 'POST',
-			endpoint: "https://us.lightify-api.org",
+			uri: "https://us.lightify-api.org",
 			path: "/lightify/services/session",
-			body: [username: getUsername(), password: getPassword(), serialNumber : getSerial()],
-			headers: ['Content-Type': 'application/json'],
-			//requestContentType: "application/x-www-form-urlencoded",
-			synchronous: true
+			body: [
+				//'username': getUsername(),
+                //'password': getPassword(),
+                //'serialNumber': getSerial()
+                'username': 'mike@sheltonstudios.net',
+                'password': 'yOp&GarTe#m3Cxcp',
+                'serialNumber': 'OSR027CAE67'
+            ],
+			headers: [
+            	'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            ]
+			//synchronous: true
     ]
-	def loginRequest = new physicalgraph.device.HubAction(httpRequest)
-    log.debug loginRequest
-	def loginResponse = sendHubCommand(hubAction)
-    log.debug loginResponse
-    return loginResponse
-}
-
-private loginMike2(){
-    def params = [
-        uri: "https://us.lightify-api.org",
-        //endpoint: "https://us.lightify-api.org",
-        path: "/lightify/services/session",
-        body: [
-        	'username': getUsername(),
-            'password': getPassword(),
-            'serialNumber' : getSerial()
-        ],
-        headers: [
-        	'Content-Type': 'application/json'
-		],
-        
-        contentType: 'application/json',
-        requestContentType: 'application/json'
-    ]
-
-	log.debug params
 
 	try {
-        httpPostJson(params){ resp ->
-            resp.headers.each {
-	            log.debug "${it.name} : ${it.value}"
-	        }
-	        log.debug resp
+        httpPostJson(params) { resp ->
+
+			//log.debug "in resp"
+
+			if (resp.status == 403) {
+                log.debug "Login API Call: 403 - API not responding"
+            } else if ( resp.status == 200 ){
+                //resp.headers.each {
+                //    log.debug "${it.name} : ${it.value}"
+                //}
+                //log.debug "response contentType: ${resp.contentType}"
+                log.debug "Refresh Response Data: ${resp.data}"
+
+                //Store securityToken
+                state.auth = resp.data.securityToken
+            }
+			
+            return
+            
         }
     } catch (e) {
         log.debug "something went wrong: $e"
     }
 }
+
+
+
+//[MS] Trying login different way with physicalgraph
+private loginMike(){
+}
+
+private loginMike2(){
+}
+
 private mikeRespFunction(data){
 	log.debug resp
 }
 
 private getCurrentState(deviceId){
 
-	//Split deviceId (lightify-1)
+	//Split deviceId (lightify-X)
 	def OsramID = deviceId?.split("-")[1]
 	log.debug "Getting Current State of $OsramID"
 
-	log.debug state.auth
-
-	//Custom variables are stored in state. If not defined, must log in.
-	if (state.auth == ""){
-		//state.auth = "113054-WZfAvktLpkeJhHPinrpa"
-        log.debug "no state.auth"
-        return login()
-	}
-
-    //Defaults to parse function after execution
-	rest(
+  	def params = [
 			method: 'GET',
-			endpoint: "https://us.lightify-api.org",
-			path: "/lightify/services/devices/$OsramID",
-			headers: ['Content-Type': 'application/json', 'authorization': state.auth],
-			requestContentType: "application/x-www-form-urlencoded",
-			synchronous: true
-	)
+			uri: "https://us.lightify-api.org",
+			path: "/lightify/services/devices/${OsramID}",
+			headers: [
+            	'Content-Type': 'application/json',
+                'authorization': state.auth
+            ]
+    ]
+
+	try {
+        httpGet(params) { resp ->
+			if (resp.status == 403) {
+                log.debug "Get API Call: 403 - API not responding"
+            } else if ( resp.status == 200 ){
+                log.debug "response data: ${resp.data}"
+                log.debug "Color? ${resp.data.color}"
+
+				state.deviceStatus = resp.data
+
+                //Set some device values
+                if (resp.data.on == 1){
+                    sendEvent("name":"switch", "value": "on")
+                } else {
+                    sendEvent("name":"switch", "value": "off")
+                }
+                sendEvent("name":"level", "value": (resp.data.brightnessLevel *100 ))
+			}
+			
+            return
+            
+        }
+    } catch (e) {
+        log.debug "something went wrong: $e"
+    }
 }
 
 
 //[MS] My parse
 def parse (Map response) {
-
-	log.debug "in parse"
-
-	log.debug response
-    log.debug response.data.brightnessLevel
-
-	def result = []
-
-	if (response.status == 403) {
-		//Server not responding
-        log.debug "403 - API not responding"
-    	log.debug (response.status)
-    } else if ( response.status == 400 ) {
-    	//Token invalid or expired
-        log.debug "400 - Logged out"
-		//result << login()
-		result << loginMike2()
-		log.debug "400 - after login()"
-		log.debug result
-        return
-	} else if (response.status == 200) {
-
-        log.debug "200"
-
-    	//Avoid loops
-        if ( response.data.securityToken ){
-        	state.auth = response.data.securityToken
-            log.debug "set state.auth"
-            log.debug result
-        	return
-        } else {
-	        result << response.data
-            
-            //Set some device values
-            if (response.data.on == "1"){
-				sendEvent("name":"switch", "value": "on")
-            } else {
-				sendEvent("name":"switch", "value": "off")
-            }
-			sendEvent("name":"level", "value": (response.data.brightnessLevel *100 ))
-        }
-    
-	}
-	log.debug result
-    
-    return result
-	
-/*
-	def msg = parseLanMessage(response)
-
-    def headersAsString = msg.header // => headers as a string
-    def headerMap = msg.headers      // => headers as a Map
-    def body = msg.body              // => request body as a string
-    def status = msg.status          // => http status code of the response
-    def json = msg.json              // => any JSON included in response body, as a data structure of lists and maps
-    def xml = msg.xml                // => any XML included in response body, as a document tree structure
-    def data = msg.data              // => either JSON or XML in response body (whichever is specified by content-type header in response)
-
-	log.debug (data)
-*/
-
 }
 
 
 private setSwitch(value){
+}
+
+
+def on(){
+	//Get Device
+	def deviceIdParts = device.deviceNetworkId.split("-")
+    def deviceId = deviceIdParts[1]
+	//log.debug deviceId
+    turnOnOff( deviceId, 1 )
+}
+
+def off(){
+	//Get Device
+	def deviceIdParts = device.deviceNetworkId.split("-")
+    def deviceId = deviceIdParts[1]
+	//log.debug deviceId
+    turnOnOff( deviceId, 0 )
+}
+
+//Lightify API call to turn on/off
+//Passing the level is overriden when passing onoff. It needs to be a separate call.
+private turnOnOff( deviceId, onoff ){
+	def params = [
+			method: 'GET',
+			uri: "https://us.lightify-api.org",
+			path: "/lightify/services/device/set",
+			query: [
+            	'idx': deviceId,
+                'onoff': onoff,
+                'time': getDevicePreferenceByName(device, "fade")
+            ],
+			headers: [
+            	'Content-Type': 'application/json',
+                'authorization': state.auth
+            ]
+			//synchronous: true
+    ]
+
+	try {
+        httpGet(params) { resp ->
+			if (resp.status == 403) {
+                log.debug "Login API Call: 403 - API not responding"
+            } else if ( resp.status == 200 ){
+				//Need to update device object state
+                state.deviceStatus.on = onoff
+            }
+			
+            
+            return            
+        }
+    } catch (e) {
+        log.debug "something went wrong: $e"
+    }	
+}
+
+
+//setLevel(number, number)
+
+
+def setLevel( level ){
+
+	//log.debug level
+    
+    def LightifyLevel = level/100
+
+	//Get Device
+	def deviceIdParts = device.deviceNetworkId.split("-")
+    def deviceId = deviceIdParts[1]
+
+	def params = [
+			method: 'GET',
+			uri: "https://us.lightify-api.org",
+			path: "/lightify/services/device/set",
+			query: [
+            	'idx': deviceId,
+                'level': LightifyLevel,
+                'time': getDevicePreferenceByName(device, "fade")
+            ],
+			headers: [
+            	'Content-Type': 'application/json',
+                'authorization': state.auth
+            ]
+			//synchronous: true
+    ]
+
+	try {
+        httpGet(params) { resp ->
+			if (resp.status == 403) {
+                log.debug "Login API Call: 403 - API not responding"
+            } else if ( resp.status == 200 ){
+                //log.debug "response data: ${resp.data}"
+            }
+			
+            //Need to update device object state
+			state.deviceStatus.brightnessLevel = level
+            
+            //???? How to set it on the device property?
+            //levelSliderControl.level = level
+            
+            return            
+        }
+    } catch (e) {
+        log.debug "something went wrong: $e"
+    }	
 }
